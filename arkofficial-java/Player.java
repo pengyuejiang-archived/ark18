d598be632fec08e1a6091420bcefc05b48c4a732
JOHN JIANG 2015 <johnjiang2015@pyjiang-MacBookPro.local>
2018-01-16 13:51:57 +0800

    Divided stuctures, official release is now out

---
+++ arkofficial-java/Player.java
@@ -0,0 +1,52 @@
+import bc.*;
+
+public class Player {
+
+	// Import support resources
+    public static GameController gc = new GameController();
+
+    public static void main(String[] args) {
+
+        gc.queueResearch(UnitType.Worker);
+        gc.queueResearch(UnitType.Rocket);
+        gc.queueResearch(UnitType.Ranger);
+        gc.queueResearch(UnitType.Ranger);
+        gc.queueResearch(UnitType.Ranger);
+
+		Planet planet = gc.planet();
+        while (true) {
+            System.out.println("Current round: " + gc.round());
+            hf.runStatistics();
+            VecUnit units = gc.myUnits();
+            for (int i = 0; i < units.size(); i++) {
+                Unit unit = units.get(i);
+				if (!unit.location().isOnMap()) continue;
+                switch (unit.unitType()) {
+                    case Worker:
+                        rc.runWorker(planet, unit);
+                        break;
+                    case Knight:
+                        rc.runKnight(planet, unit);
+                        break;
+                    case Ranger:
+                        rc.runRanger(planet, unit);
+                        break;
+                    case Mage:
+                        rc.runMage(planet, unit);
+                        break;
+                    case Healer:
+                        rc.runHealer(planet, unit);
+                        break;
+                    case Factory:
+                        rc.runFactory(planet, unit);
+                        break;
+                    case Rocket:
+                        rc.runFactory(planet, unit);
+                        break;
+                }
+            }
+            gc.nextTurn();
+        }
+    }
+
+}
---
+++ arkofficial-java/hf.java
@@ -0,0 +1,148 @@
+import bc.*;
+
+public class hf {
+
+	// Import gc
+	static GameController gc = Player.gc;
+
+	// I am surprised that there are actually no methods which rets the enemy team.
+    public static Team enemyOf(Team team) {
+        return team == Team.Blue ? Team.Red : Team.Blue;
+    }
+
+	// Generate a rand dir based on the num of dirs desired
+    // if 8: !include center; if 9: include center
+    public static Direction randDir(int n) {
+        int rand = (int)(Math.random() * n);
+        for (int i = 0; i < n; i++) {
+            if (i == rand) {
+                return f.dirs[i];
+            }
+        }
+        return Direction.Center;
+    }
+
+	public static Unit nearestUnit(Unit unit, VecUnit units) {
+		MapLocation uLoc = unit.location().mapLocation();
+		long min = Long.MAX_VALUE;
+		// I have to init this, it's required
+		Unit nearestUnit = unit;
+		for (int i = 0; i < units.size(); i++) {
+			MapLocation tLoc = units.get(i).location().mapLocation();
+			if (uLoc.distanceSquaredTo(tLoc) < min) {
+				min = uLoc.distanceSquaredTo(tLoc);
+				nearestUnit = units.get(i);
+			}
+		}
+		return nearestUnit;
+	}
+
+	// A primative path-finding method.
+    public static void findPathTo(Unit unit, MapLocation dest) {
+        MapLocation src = unit.location().mapLocation();
+        long min = src.distanceSquaredTo(dest);
+        Direction optDir = Direction.Center;
+        long temp;
+        for (int i = 0; i < 8; i++) {
+            MapLocation tempLoc = src.add(f.dirs[i]);
+            temp = tempLoc.distanceSquaredTo(dest);
+            // Refresh min distance:
+            if (temp < min && gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), f.dirs[i])) {
+                min = temp;
+                optDir = f.dirs[i];
+            }
+        }
+        if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), optDir)) {
+            gc.moveRobot(unit.id(), optDir);
+        }
+    }
+
+    // The counterpart of findPathTo(), having the exact same architecture.
+    public static void runAwayFrom(Unit unit, MapLocation loc) {
+        MapLocation uLoc = unit.location().mapLocation();
+        long max = uLoc.distanceSquaredTo(loc);
+        Direction optDir = Direction.Center;
+        long temp;
+        for (int i = 0; i < 8; i++) {
+            MapLocation tempLoc = uLoc.add(f.dirs[i]);
+            temp = tempLoc.distanceSquaredTo(loc);
+            // Refresh min distance:
+            if (temp > max && gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), f.dirs[i])) {
+                max = temp;
+                optDir = f.dirs[i];
+            }
+        }
+        if (gc.isMoveReady(unit.id()) && gc.canMove(unit.id(), optDir)) {
+            gc.moveRobot(unit.id(), optDir);
+        }
+    }
+
+    // Self-explanatory methods for statistics.
+    public static void runStatistics() {
+        clrStatistics();
+        VecUnit units = gc.myUnits();
+        for (int i = 0; i < units.size(); i++) {
+            Unit unit = units.get(i);
+            switch (unit.unitType()) {
+                case Worker:
+                    f.workerCount++;
+                    break;
+                case Knight:
+                    f.knightCount++;
+                    break;
+                case Ranger:
+                    f.rangerCount++;
+                    break;
+                case Mage:
+                    f.mageCount++;
+                    break;
+                case Healer:
+                    f.healerCount++;
+                    break;
+                case Factory:
+                    f.factoryCount++;
+                    break;
+                case Rocket:
+                    f.rocketCount++;
+                    break;
+            }
+        }
+    }
+
+	private static void clrStatistics() {
+		f.workerCount = 0;
+		f.knightCount = 0;
+		f.rangerCount = 0;
+		f.mageCount = 0;
+		f.healerCount = 0;
+		f.factoryCount = 0;
+		f.rocketCount = 0;
+	}
+
+	public static void reportELoc(MapLocation eLoc) {
+		if (gc.planet() == Planet.Earth) {
+			f.eLocEarthInitialized = true;
+			f.eLocEarth = eLoc;
+		} else {
+			f.eLocMarsInitialized = true;
+			f.eLocMars = eLoc;
+		}
+	}
+
+	public static MapLocation getELoc(Planet planet) {
+		if (planet == Planet.Earth) {
+			return f.eLocEarth;
+		} else {
+			return f.eLocMars;
+		}
+	}
+
+	public static boolean eLocInitialized(Planet planet) {
+		if (planet == Planet.Earth) {
+			return f.eLocEarthInitialized;
+		} else {
+			return f.eLocMarsInitialized;
+		}
+	}
+
+}
