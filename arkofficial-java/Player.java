87d66f34a5569a2ac85c11c71791491edf2e0194
JOHN JIANG 2015 <johnjiang2015@pyjiang-MacBookPro.local>
2018-01-24 16:17:25 +0800

    rearranged research queue + can handle exception + operation time message

--- arkofficial-java/Player.java
+++ arkofficial-java/Player.java
@@ -6,50 +6,58 @@ public class Player {
     public static GameController gc = new GameController();
 
     public static void main(String[] args) {
+		try {
 
-        gc.queueResearch(UnitType.Worker);
-        gc.queueResearch(UnitType.Rocket);
-		gc.queueResearch(UnitType.Rocket);
-		gc.queueResearch(UnitType.Rocket);
-		gc.queueResearch(UnitType.Ranger);
-		gc.queueResearch(UnitType.Ranger);
-		gc.queueResearch(UnitType.Ranger);
+			// Research queue:
+			gc.queueResearch(UnitType.Worker);
+	        gc.queueResearch(UnitType.Rocket);
+			gc.queueResearch(UnitType.Rocket);
+			gc.queueResearch(UnitType.Ranger);
+			gc.queueResearch(UnitType.Rocket);
+			gc.queueResearch(UnitType.Ranger);
+			gc.queueResearch(UnitType.Ranger);
 
-		Planet planet = gc.planet();
-        while (true) {
-            System.out.println("Current round: " + gc.round());
-            VecUnit units = gc.myUnits();
-			hf.runStatistics();
-			f.unitCount = (int)units.size();
-            for (int i = 0; i < units.size(); i++) {
-                Unit unit = units.get(i);
-				if (!unit.location().isOnMap()) continue;
-                switch (unit.unitType()) {
-                    case Worker:
-                        rc.runWorker(planet, unit);
-                        break;
-                    case Knight:
-                        rc.runKnight(planet, unit);
-                        break;
-                    case Ranger:
-                        rc.runRanger(planet, unit);
-                        break;
-                    case Mage:
-                        rc.runMage(planet, unit);
-                        break;
-                    case Healer:
-                        rc.runHealer(planet, unit);
-                        break;
-                    case Factory:
-                        rc.runFactory(planet, unit);
-                        break;
-                    case Rocket:
-                        rc.runRocket(planet, unit);
-                        break;
-                }
-            }
-            gc.nextTurn();
-        }
+			Planet planet = gc.planet();
+	        while (true) {
+	            System.out.println("Current round: " + gc.round());
+				System.out.println("Time left: " + gc.getTimeLeftMs());
+	            VecUnit units = gc.myUnits();
+				hf.runStatistics();
+				f.unitCount = (int)units.size();
+	            for (int i = 0; i < units.size(); i++) {
+	                Unit unit = units.get(i);
+					if (!unit.location().isOnMap()) continue;
+	                switch (unit.unitType()) {
+	                    case Worker:
+	                        rc.runWorker(planet, unit);
+	                        break;
+	                    case Knight:
+	                        rc.runKnight(planet, unit);
+	                        break;
+	                    case Ranger:
+	                        rc.runRanger(planet, unit);
+	                        break;
+	                    case Mage:
+	                        rc.runMage(planet, unit);
+	                        break;
+	                    case Healer:
+	                        rc.runHealer(planet, unit);
+	                        break;
+	                    case Factory:
+	                        rc.runFactory(planet, unit);
+	                        break;
+	                    case Rocket:
+	                        rc.runRocket(planet, unit);
+	                        break;
+	                }
+	            }
+	            gc.nextTurn();
+	        }
+			
+		} catch (Exception e) {
+			System.out.println("I've got an exception!");
+			e.printStackTrace();
+		}
     }
 
 }
